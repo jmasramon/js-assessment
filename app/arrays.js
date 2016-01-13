d2e433b3714107acc45f5aaf602b3521e5710cff
jmasramon <masramon@qumram.com>
2016-01-13 11:10:19 +0100

    first 3 array ones

--- app/arrays.js
+++ app/arrays.js
@@ -3,15 +3,45 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum : function(arr) {
+    var sum = 0,
+        i = arr.length;
+    // for (var index in arr) {
+    //   sum += parseInt(arr[index]);
+    // }
 
+    // for (i = arr.length; i--; ) {
+    //   sum += parseInt(arr[i]);
+    // }
+
+    // while (i--) {
+    //   sum += parseInt(arr[i]);
+    // }
+
+    sum  = arr.reduce(function (prev, cur) {
+      return prev + cur;
+    }, 0);
+
+    return sum;
   },
 
   remove : function(arr, item) {
+    var res = arr,
+        i;
+    // while((i = res.indexOf(item)) >-1){
+    //   res.splice(i,1);
+    //   i = res.indexOf(item);
+    // }
 
+    res = res.filter(function (elem) {
+      if (elem === item) return false;
+      return true;
+    });
+
+    return res;
   },
 
   removeWithoutCopy : function(arr, item) {
