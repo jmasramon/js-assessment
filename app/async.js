0ff70477cd1a22df2a3efc5a39ae17b5af0e24b6
Jordi Masramon Solans <jordi.masramon@gmail.com>
2016-01-26 09:19:05 +0100

    some more

--- app/async.js
+++ app/async.js
@@ -1,11 +1,42 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
-  async : function(value) {
+    async: function(value) {
+        return new Promise(function(resolve, reject) {
+            resolve(value);
+        });
+    },
 
-  },
+    manipulateRemoteData: function(url) {
+        var res = new Promise(function(resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
 
-  manipulateRemoteData : function(url) {
+            xhr.onreadystatechange = function(e) {
+                if (xhr.readyState !== 4) {
+                    return;
+                }
 
-  }
+                if ([200, 304].indexOf(xhr.status) === -1) {
+                    reject(new Error('Server responded with a status of ' + xhr.status));
+                } else {
+                    var resp = e.target.response;
+                    var result = [];
+                    var people = JSON.parse(resp).people;
+                    console.log('resolved to ', people);
+
+                    for (key in people) {
+                        if (people.hasOwnProperty(key)) {
+                            result.push(people[key].name);
+                        }
+                    }
+                    result.sort();
+                    resolve(result);
+                }
+            };
+
+            xhr.send();
+        });
+        return res;
+    }
 };
--- app/numbers.js
+++ app/numbers.js
@@ -1,19 +1,22 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
-  valueAtBit: function(num, bit) {
+    valueAtBit: function(num, bit) {
+        var binaryString = (num >>> 0).toString(2);
+        return parseInt(binaryString[binaryString.length - bit], 10);
+        ///10000000
+    },
 
-  },
+    base10: function(str) {
+        return parseInt(str, 2);
+    },
 
-  base10: function(str) {
+    convertToBinary: function(num) {
+        var binaryString = Number(num).toString(2);
+        return '00000000'.substr(binaryString.length) + binaryString;
+    },
 
-  },
-
-  convertToBinary: function(num) {
-
-  },
-
-  multiply: function(a, b) {
-
-  }
+    multiply: function(a, b) {
+        return parseFloat((a * b).toFixed(b.toString().length - 2));
+    }
 };
